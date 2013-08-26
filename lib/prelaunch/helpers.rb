f895f6cf8f8aa98ba1baec03d3bb136306b718c5
Alexey Chernetsov <chernetsov0@gmail.com>
2013-08-26 03:15:45 +0300

    Fix helper to render correct URL on empty path prefix

--- lib/prelaunch/helpers.rb
+++ lib/prelaunch/helpers.rb
@@ -3,14 +3,21 @@ module Prelaunch
     def prelaunch_logout_link name = nil, html_options = nil, &block
       return unless Prelaunch::valid_env?
 
-      url = "#{Prelaunch.path}/logout"
+      url = "#{strip_slashes(Prelaunch.path)}/logout"
 
       html_options = name if block_given?
       html_options ||= {}
-      html_options[:rel ] = 'nofollow'
+      html_options[:rel] = 'nofollow'
       html_options['href'] ||= url
 
       content_tag(:a, name || url, html_options, &block)
     end
+
+    private
+      def strip_slashes string
+        result = string.gsub(/^(\/|\\)+/, '').gsub(/(\/|\\)+$/, '')
+
+        result.length > 0 ? '/' + result : result
+      end
   end
 end
